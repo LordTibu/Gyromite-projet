11264be04014dff6ae6536fd8f1297c834e4fd9f
Lenis <jhomilo47@gmail.com>
2022-11-30 16:31:04 +0100

    Patino es mka y q las cosas no se llamen main lo altera

--- src/main/java/modele/deplacements/Colonne.java
+++
@@ -1,9 +0,0 @@
-package modele.deplacements;
-
-/**
- * A la reception d'une commande, toutes les cases (EntitesDynamiques) des colonnes se déplacent dans la direction définie
- * (vérifier "collisions" avec le héros)
- */
-public class Colonne extends RealisateurDeDeplacement {
-    protected boolean realiserDeplacement() { return false; } // TODO
-}
